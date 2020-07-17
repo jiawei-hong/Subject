948bef0baf038e438d8d0f9758df955ff50645bb
jiawe11 <zxc37984@gmail.com>
2020-07-17 11:52:40 +0800

    first commit

--- py/test.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-import time
-
-while (True):
-    browser = webdriver.Chrome()
-    browser.get('http://localhost:8000/category/1')
-    time.sleep(3)
-    browser.close()
